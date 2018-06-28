14e2b79b2d6ba7b9322061d1782d942adec5d298
Peter Monks <pmonks@gmail.com>
2018-06-28 11:54:27 -0700

    :gem: Roll version number in preparation for 0.2.0 development

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
 ; limitations under the License.
 ;
 
-(defproject org.clojars.pmonks/clj-2253 "0.1.0"
+(defproject org.clojars.pmonks/clj-2253 "0.2.0-SNAPSHOT"
   :description         "A workaround for https://dev.clojure.org/jira/browse/CLJ-2253"
   :url                 "https://github.com/pmonks/CLJ-2253"
   :license             {:spdx-license-identifier "Apache-2.0"
